4dc1c452dc19849a88f2bac43e5ebbbd81edbd95
ehime <dodomeki@gmail.com>
2014-09-16 09:10:50 -0700

    Customer transformations from a one off database type

---
+++ php/classes/pdoBinder.php
@@ -0,0 +1,232 @@
+<?php
+
+class PDOConfig Extends PDO 
+{
+    private $engine;
+    private $host;
+    private $database;
+    private $user;
+    private $pass;
+   
+    public function __construct()
+    {
+        $this->engine   = 'mysql';
+        $this->host     = 'localhost';
+        $this->database = '';
+        $this->user     = 'root';
+        $this->pass     = '';
+        $dns = $this->engine.':dbname='.$this->database.";host=".$this->host;
+        parent::__construct( $dns, $this->user, $this->pass );
+    }
+}
+
+
+Class Connection
+{
+    public  $slice      = 0;
+
+    private $total      = 0,
+            $firstId    = 0,
+            $lastId     = 0;
+
+    private $map        = [];
+
+    public function __construct()
+    {
+        $this->dbh = New \PDOConfig();
+        $this->dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
+    }
+
+    public function display($type = false)
+    {
+        if (isset($this->$type)) print_r($this->$type);
+
+            return $this;
+    }
+
+    public function setTotals()
+    {
+        $res = $this->dbh->query('SELECT count(id) AS total FROM wds_user');
+        $res->execute();
+
+        $obj = $res->fetchAll(PDO::FETCH_CLASS, 'ArrayObject') [0];
+
+            $this->total = $obj->total;
+
+        $res = $this->dbh->query('SELECT id AS firstId FROM wds_user ORDER BY id ASC LIMIT 1');
+        $res->execute();
+
+        $obj = $res->fetchAll(PDO::FETCH_CLASS, 'ArrayObject') [0];
+
+            $this->firstId = $obj->firstId;
+
+        $res = $this->dbh->query('SELECT id AS lastId FROM wds_user ORDER BY id DESC LIMIT 1');
+        $res->execute();
+
+        $obj = $res->fetchAll(PDO::FETCH_CLASS, 'ArrayObject') [0];
+
+            $this->lastId = $obj->lastId;
+
+        return $this;
+    }
+
+    public function chunk($range = 3)
+    {
+        $i = $this->firstId; //2
+
+        while (true)
+        {
+            $c = ($i + $range);
+            if ($this->lastId < $c) 
+            {
+                $this->map[] = [
+                    (int) $i, 
+                    (int) $this->lastId]
+                ;
+
+                return $this;
+            }
+
+            $this->map[] = [
+                (int) $i, 
+                (int) $c
+            ]; 
+
+            $i = $c++;
+        }
+    }
+
+    public function getCustomers()
+    {
+        if (isset($this->map[$this->slice]))
+        {
+            echo "\nSlice: {$this->map[$this->slice][0]}, {$this->map[$this->slice][1]}\n\n";
+
+            $res = $this->dbh->prepare(trim(
+                'SELECT 
+                    ins_by          AS userid,
+                    wds_user_id     AS customer, 
+                    wds_address_id  AS billing,
+                    (
+                        SELECT wds_address_id  AS shipping
+                        FROM wds_user_address B
+                        WHERE ins_by != "GUEST" AND wds_address_type_id = 2 AND B.ins_by = A.ins_by
+                    ) AS shipping
+
+                FROM 
+
+                    wds_user_address A 
+
+                WHERE ins_by != "GUEST" AND wds_address_type_id = 1
+
+                 ORDER BY id ASC LIMIT :low, :high'
+             ));
+
+
+            $res->bindParam(':low',  $this->map[$this->slice][0], PDO::PARAM_INT);
+            $res->bindParam(':high', $this->map[$this->slice][1], PDO::PARAM_INT);
+            $res->execute();
+
+            $this->map[$this->slice] = $res->fetchAll(PDO::FETCH_OBJ);
+
+            $this->getCustomerLists();
+        }
+        return $this;
+    }
+
+
+    protected function getCustomerLists()
+    {
+        if (isset($this->map[$this->slice]) && ! empty($this->map[$this->slice]))
+        {
+            foreach ($this->map[$this->slice] AS $id => &$object)
+            {
+                $res = $this->dbh->prepare(trim(
+                    'SELECT 
+                        ""              AS prefix,
+                        ""              AS suffix,
+                        id,
+                        active_bl   AS active,
+                        company_id  AS corpId,
+                        email,
+                        password,
+                        first_name  AS firstname,
+                        middle_name AS middlename,
+                        last_name   AS lastname
+
+                     FROM 
+
+                        wds_user
+
+                     WHERE
+
+                        id = :id'
+                ));
+
+                $res->bindParam(':id',  $object->customer, PDO::PARAM_INT);
+                $res->execute();
+
+                $object->customer = $res->fetchAll(PDO::FETCH_OBJ) [0];
+
+                $res = $this->dbh->prepare(
+                    'SELECT 
+                        id,
+                        company,
+                        street1,
+                        street2,
+                        street3,
+                        city,
+                        state,
+                        postal_code AS postal,
+                        country_id  AS countryId
+                    FROM 
+                        wds_address 
+                    WHERE 
+                        id = :id'
+                );
+
+                $res->bindParam(':id',  $object->billing, PDO::PARAM_INT);
+                $res->execute();
+                $object->billing = $res->fetchAll(PDO::FETCH_OBJ) [0];
+
+                $res = $this->dbh->prepare(
+                    'SELECT 
+                        id,
+                        company,
+                        street1,
+                        street2,
+                        street3,
+                        city,
+                        state,
+                        postal_code AS postal,
+                        country_id  AS countryId
+                    FROM 
+                        wds_address 
+                    WHERE 
+                        id = :id'
+                );
+
+                $res->bindParam(':id',  $object->shipping, PDO::PARAM_INT);
+                $res->execute();
+
+                $object->shipping = $res->fetchAll(PDO::FETCH_OBJ) [0];
+            }
+        }
+        return $this;
+    }
+
+    public function getSlice()
+    {
+        return $this->map[$this->slice];
+    }
+
+    public function set($type)
+    {
+        $this->$type = $this->map[$this->slice];
+        $this->slice++;
+
+        return (isset($this->map[$this->slice]) && ! empty($this->map[$this->slice])) 
+            ? $this
+            : false;
+    }
+}
---
+++ php/mmci.php
@@ -0,0 +1,158 @@
+<?php
+
+  /** 
+   * Magento Mass Customer Import
+   * CPR : ehime :: Jd Daniel
+   * MOD : 2014-09-09 @ 15:22:17
+   * VER : 1.0
+   * 
+   * DEP : Mage.php
+   * Magento core action file
+   *
+   * DEP : pdoFinder.php
+   * Requires access to a source database
+   */
+
+  define('MAGENTO', realpath(dirname(__FILE__)));
+  require_once MAGENTO . '/../app/Mage.php';
+
+  umask(0);
+  Mage::app();
+
+  require_once 'classes/pdoBinder.php';
+
+  $customer = Mage::getModel("customer/customer");
+  $address  = Mage::getModel("customer/address");
+
+  $conn = New \Connection();
+  $conn = $conn->setTotals()->chunk()->getCustomers()->set('cu');
+
+  while(true)
+  {
+    foreach ($conn->cu AS $cu) 
+    {
+      $webid    = Mage::app()->getWebsite()->getId();
+      $store    = Mage::app()->getStore();
+
+      $customer->loadByEmail($cu->email);
+    
+      // if customer does not already exists, by email
+      if (! $customer->getId()) 
+      {
+
+        $customer->setWebsiteId($wid)
+                 ->setStore($store)
+                 //->setGroupId(2)
+                 ->setPrefix        ($cu->customer->prefix)
+                 ->setFirstname     ($cu->customer->firstname)
+                 ->setMiddleName    ($cu->customer->middlename)
+                 ->setLastname      ($cu->customer->lastname)
+                 ->setSuffix        ($cu->customer->suffix)
+                 ->setEmail         ($cu->customer->email);
+
+        // generate a new password
+        $customer->changePassword(
+          $customer->generatePassword()
+        );
+
+      } else {
+       // do something here for existing customers
+      }
+
+      try 
+      {
+
+        // ?? $customer->save()->setConfirmation(null)->save();
+        $customer->setConfirmation(null)
+                 ->save();
+
+        // save successful, send new password
+        // uncomment this to send the email to the customer
+        // $customer->sendPasswordReminderEmail();
+
+      } catch (Exception $e) {
+        Zend_Debug::dump($e->getMessage());
+      }
+
+
+      // Set biilling address
+      $regionModel = Mage::getModel('directory/region')
+                         ->loadByCode(
+                            $cu->state, 
+                            $cu->billing->countryId
+                         );
+
+      $rid = $regionModel->getId();
+
+      $address->setCustomerId         ($customer->getId())
+              ->setFirstname          ($customer->getFirstname())
+              ->setMiddleName         ($customer->getMiddlename())
+              ->setLastname           ($customer->getLastname())
+            
+              ->setRegionId           ($rid)            //sta  ->setCountryId          ($cid)te/province, only needed if the country is USA
+              ->setPostcode           ($cu->billing->postal)
+              ->setCity               ($cu->billing->city)
+              ->setTelephone          ($cu->billing->phone)
+              ->setCompany            ($cu->billing->company)
+
+              ->setData               ('street' => [
+                                        $cu->billing->street1, 
+                                        $cu->billing->street2, 
+                                        $cu->billing->street3,
+                                      ])
+
+              ->setIsDefaultBilling   ('1')
+//              ->setIsDefaultShipping  ('1')
+              ->setSaveInAddressBook  ('1');
+
+      try {
+        $address->save();
+      }
+
+      catch (Exception $e) {
+        Zend_Debug::dump($e->getMessage());
+      }
+
+      // Set shipping address
+      $regionModel = Mage::getModel('directory/region')
+                         ->loadByCode(
+                            $cu->state, 
+                            $cu->shipping->countryId
+                         );
+
+      $rid = $regionModel->getId();
+
+      $address->setCustomerId         ($customer->getId())
+              ->setFirstname          ($customer->getFirstname())
+              ->setMiddleName         ($customer->getMiddlename())
+              ->setLastname           ($customer->getLastname())
+            
+              ->setRegionId           ($rid)            //sta  ->setCountryId          ($cid)te/province, only needed if the country is USA
+              ->setPostcode           ($cu->shipping->postal)
+              ->setCity               ($cu->shipping->city)
+              ->setTelephone          ($cu->shipping->phone)
+              ->setCompany            ($cu->shipping->company)
+
+              ->setData               ('street' => [
+                                        $cu->shipping->street1, 
+                                        $cu->shipping->street2, 
+                                        $cu->shipping->street3,
+                                      ])
+
+//              ->setIsDefaultBilling   ('1')
+              ->setIsDefaultShipping  ('1')
+              ->setSaveInAddressBook  ('1');
+
+      try {
+        $address->save();
+      }
+
+      catch (Exception $e) {
+        Zend_Debug::dump($e->getMessage());
+      }
+    }
+
+    $conn = $conn->getCustomers()->set('cu');
+
+      if (! $conn) break;
+  }
